a7fada3d7485b351d4c51a9859605e38554924c2
Alan Mishchenko <alanmi@eecs.berkeley.edu>
2012-09-15 23:27:46 -0700

    Created new abstraction package from the code that was all over the place.

--- src/proof/abs/absRef.h
+++ src/proof/abs/absRef.h
@@ -1,10 +1,10 @@
 /**CFile****************************************************************
 
-  FileName    [giaAbsRef2.h]
+  FileName    [absRef.h]
 
   SystemName  [ABC: Logic synthesis and verification system.]
 
-  PackageName [Scalable AIG package.]
+  PackageName [Abstraction package.]
 
   Synopsis    [Refinement manager.]
 
@@ -14,12 +14,12 @@
 
   Date        [Ver. 1.0. Started - June 20, 2005.]
 
-  Revision    [$Id: giaAbsRef2.h,v 1.00 2005/06/20 00:00:00 alanmi Exp $]
+  Revision    [$Id: absRef.h,v 1.00 2005/06/20 00:00:00 alanmi Exp $]
 
 ***********************************************************************/
  
-#ifndef ABC__aig__gia__giaAbsRef2_h
-#define ABC__aig__gia__giaAbsRef2_h
+#ifndef ABC__proof_abs__AbsRef_h
+#define ABC__proof_abs__AbsRef_h
 
 
 ////////////////////////////////////////////////////////////////////////
@@ -37,7 +37,7 @@ ABC_NAMESPACE_HEADER_START
 ///                         BASIC TYPES                              ///
 ////////////////////////////////////////////////////////////////////////
 
-typedef struct Rf2_Man_t_ Rf2_Man_t; // refinement manager
+typedef struct Rnm_Man_t_ Rnm_Man_t; // refinement manager
 
 ////////////////////////////////////////////////////////////////////////
 ///                      MACRO DEFINITIONS                           ///
@@ -48,10 +48,10 @@ typedef struct Rf2_Man_t_ Rf2_Man_t; // refinement manager
 ////////////////////////////////////////////////////////////////////////
 
 /*=== giaAbsRef.c ===========================================================*/
-extern Rf2_Man_t *  Rf2_ManStart( Gia_Man_t * pGia );
-extern void         Rf2_ManStop( Rf2_Man_t * p, int fProfile );
-extern double       Rf2_ManMemoryUsage( Rf2_Man_t * p );
-extern Vec_Int_t *  Rf2_ManRefine( Rf2_Man_t * p, Abc_Cex_t * pCex, Vec_Int_t * vMap, int fPropFanout, int fVerbose );
+extern Rnm_Man_t *  Rnm_ManStart( Gia_Man_t * pGia );
+extern void         Rnm_ManStop( Rnm_Man_t * p, int fProfile );
+extern double       Rnm_ManMemoryUsage( Rnm_Man_t * p );
+extern Vec_Int_t *  Rnm_ManRefine( Rnm_Man_t * p, Abc_Cex_t * pCex, Vec_Int_t * vMap, int fPropFanout, int fPostProcess, int fVerbose );
 
 
 
